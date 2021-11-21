9519a5be6d3c136397f5c54206ac3f16a0c40ddd
MohamedKerkeb <kerkeb.moha@gmail.com>
2021-11-21 23:36:50 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,42 @@
 import { Component } from '@angular/core';
+import { rejects } from 'assert';
+import { resolve } from 'dns';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'opentuto';
+  isAuth = false;
+  lastUpdate = new Promise((resolve, reject) => {
+    const date = new Date();
+    setTimeout(() => {
+      resolve(date);
+    }, 2000);
+  });
+
+  appareils = [
+    {
+      name: 'Machine à laver',
+      status: 'allumé',
+    },
+    {
+      name: 'Télévision',
+      status: 'allumé',
+    },
+    {
+      name: 'Ordinateur',
+      status: 'éteint',
+    },
+  ];
+
+  constructor() {
+    setTimeout(() => {
+      this.isAuth = true;
+    }, 4000);
+  }
+  onAllumer() {
+    console.log('on allume tout!');
+  }
 }
